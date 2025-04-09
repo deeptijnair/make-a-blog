0e4bd8e59b18e52413b3e9db0d6b9df14d59c0bf
Deepthi <11665833+deeptijnair@users.noreply.github.com>
2025-04-08 22:50:05 -0400

    Added useState to trigger the change in the browser.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [name, setName] = useState('Deepthi');
+
+ //   let name = 'Deepthi';
+
+    const handleClick = () => {
+        setName('India');
+        console.log(name);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
